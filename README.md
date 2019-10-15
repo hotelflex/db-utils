839ee968dc22957f39a15ef1d490e5e77f5b150d
Rich Turnbull <rpturnbull1@gmail.com>
2019-10-15 14:06:41 +0100

    moved to flexfactory

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # db-utils
+
+## Install
+```
+npm i --save @flexfactory/db-utils
+```
+
+## Usage
+getPGClient
+```javascript
+const { getPGClient } = require('@flexfactory/db-utils');
+
+const client = getPGClient({
+  database: process.env.DATABASE_NAME,
+  host: process.env.POSTGRES_HOST,
+  user: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+});
+```
--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
-const pg = require('pg')
-const knex = require('knex')
-const Errors = require('./Errors')
+const pg = require('pg');
 
-pg.types.setTypeParser(20, 'text', parseInt)
-pg.types.setTypeParser(1700, 'text', parseFloat)
+pg.types.setTypeParser(20, 'text', parseInt);
+pg.types.setTypeParser(1700, 'text', parseFloat);
 
-module.exports.Errors = Errors
-
-module.exports.createTransaction = knex => {
-  return new Promise(resolve => {
-    return knex.transaction(resolve)
-  })
-}
-
-module.exports.getKnexClient = (postgres, { min, max } = {}, connOpts = {}) => {
-  return knex({
-    client: 'pg',
-    connection: Object.assign({}, postgres, connOpts),
-    pool: { min, max },
-  })
-}
-
-module.exports.getPGClient = postgres => {
-  const client = new pg.Client(postgres)
-  client.connect()
-  return client
+module.exports.getPGClient = (config) => {
+  const client = new pg.Client(config);
+  client.connect();
+  return client;
 }
